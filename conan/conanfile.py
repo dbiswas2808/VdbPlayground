f4bfaf2a1fe5329388b95c2cf8bb1e6cd091d26a
dbiswas2808 <dbiswas2808@gmail.com>
2023-06-15 18:12:49 -0400

    Adding conan to the repo that inlcudes important dependencies

---
+++ conan/conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile, CMake
+
+class TestProject(ConanFile):
+    name = "Test"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake", "cmake_find_package"
+    # exports_sources = "../*"
+
+    def requirements(self):
+        self.requires("eigen/3.4.0")
+        self.requires("boost/1.81.0")
+        self.requires("jemalloc/5.3.0")
+
+    def build(self):
+        cmake = CMake(self)
+        # cmake.configure(source_folder="src")
+        cmake.build()
